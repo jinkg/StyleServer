1031509db3421b3ca590d5e2bf9a8ccb848e1b96
jinyalin <jinyalin@kanzhun.com>
2017-01-03 19:56:51 +0800

    add resource file

--- src/main/java/Style.java
+++ src/main/java/Style.java
@@ -1,11 +1,13 @@
 
 import com.google.gson.Gson;
 import model.WallpaperItem;
+import spark.Spark;
 
 import java.util.*;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
+import static spark.Spark.staticFileLocation;
 
 /**
  * 34921
@@ -13,8 +15,10 @@ import static spark.Spark.port;
  */
 public class Style {
     public static void main(String[] args) {
+        staticFileLocation("/public");
         port(6060);
         get("/style", (request, response) -> getStyle());
+
     }
 
     private static final String DATA_KEY_WALLPAPER = "wallpapers";
@@ -25,14 +29,14 @@ public class Style {
         List<WallpaperItem> items = new ArrayList<>();
 
         items.add(new WallpaperItem(UUID.randomUUID().toString(),
-                "http://www.tactic.at/wp-content/gallery/herbst-im-salzkammergut-wallpaper/herbst-im-salzkammergut-wallpaper-1.jpg",
-                "风景", "Yalin", "attribution"));
-        items.add(new WallpaperItem(UUID.randomUUID().toString(),
-                "http://wallpapersqq.net/wp-content/uploads/2015/12/Gintoki-Sakata-abstract.jpg",
-                "死神", "Yalin", "attribution"));
-        items.add(new WallpaperItem(UUID.randomUUID().toString(),
-                "http://labs3.kentooz.com/simplex/wp-content/uploads/2013/04/windows-vista-wallpaper-182.jpg",
-                "蓝色", "Yalin", "attribution"));
+                "http://172.16.3.189:6060/20170102.jpg",
+                "风景", "Theo van Rysselberghe, 1892", "wikiart.org"));
+//        items.add(new WallpaperItem(UUID.randomUUID().toString(),
+//                "http://wallpapersqq.net/wp-content/uploads/2015/12/Gintoki-Sakata-abstract.jpg",
+//                "死神", "Yalin", "attribution"));
+//        items.add(new WallpaperItem(UUID.randomUUID().toString(),
+//                "http://labs3.kentooz.com/simplex/wp-content/uploads/2013/04/windows-vista-wallpaper-182.jpg",
+//                "蓝色", "Yalin", "attribution"));
 
         dataMap.put(DATA_KEY_WALLPAPER, items);
 
