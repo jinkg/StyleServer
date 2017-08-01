466d87b55c3465b29331dcaecaefafcc37daed24
jinyalin <jinyalin@baidu.com>
2017-08-01 13:58:50 +0800

    add rainbow

--- src/main/java/com/style/server/parser/AdvanceWallpaperParser.java
+++ src/main/java/com/style/server/parser/AdvanceWallpaperParser.java
@@ -12,42 +12,55 @@ import java.util.UUID;
  * on 2017/7/28.
  */
 public class AdvanceWallpaperParser {
+    private static final String RES_HOST = "http://172.22.158.86:6060";
+
     public static List<AdvanceWallpaperItem> parseToList() {
         List<AdvanceWallpaperItem> wallpapers = new ArrayList<>();
 
-        AdvanceWallpaperItem item = new AdvanceWallpaperItem();
-        item.name = "旋转立方";
-        item.wallpaperId = UUID.randomUUID().toString();
-        item.link = "http://www.kinglloy.com/";
-        item.author = "Yalin";
-        item.iconUrl = "http://172.22.158.86:6060/advance/icons/cube.png";
-        item.downloadUrl = "http://172.22.158.86:6060/advance/components/cube.component";
-        item.checkSum = ChecksumUtil.getChecksum("./wallpapers/advance/components/cube.component");
-        item.providerName = "com.yalin.component1.ProviderImpl";
+        AdvanceWallpaperItem cubeItem = new AdvanceWallpaperItem();
+        cubeItem.name = "旋转立方";
+        cubeItem.wallpaperId = UUID.randomUUID().toString();
+        cubeItem.link = "http://www.kinglloy.com/";
+        cubeItem.author = "Yalin";
+        cubeItem.iconUrl = RES_HOST + "/advance/icons/cube.png";
+        cubeItem.downloadUrl = RES_HOST + "/advance/components/cube.component";
+        cubeItem.checkSum = ChecksumUtil.getChecksum("./wallpapers/advance/components/cube.component");
+        cubeItem.providerName = "com.yalin.component1.ProviderImpl";
 
-        AdvanceWallpaperItem item2 = new AdvanceWallpaperItem();
-        item2.name = "绿色草地";
-        item2.wallpaperId = UUID.randomUUID().toString();
-        item2.link = "http://www.kinglloy.com/";
-        item2.author = "Yalin";
-        item2.iconUrl = "http://172.22.158.86:6060/advance/icons/the_blue.png";
-        item2.downloadUrl = "http://172.22.158.86:6060/advance/components/blue.component";
-        item2.checkSum = ChecksumUtil.getChecksum("./wallpapers/advance/components/blue.component");
-        item2.providerName = "com.yalin.component2.ProviderImpl";
+        AdvanceWallpaperItem blueItem = new AdvanceWallpaperItem();
+        blueItem.name = "绿色草地";
+        blueItem.wallpaperId = UUID.randomUUID().toString();
+        blueItem.link = "http://www.kinglloy.com/";
+        blueItem.author = "Yalin";
+        blueItem.iconUrl = RES_HOST + "/advance/icons/the_blue.png";
+        blueItem.downloadUrl = RES_HOST + "/advance/components/blue.component";
+        blueItem.checkSum = ChecksumUtil.getChecksum("./wallpapers/advance/components/blue.component");
+        blueItem.providerName = "com.yalin.component2.ProviderImpl";
 
-        AdvanceWallpaperItem item3 = new AdvanceWallpaperItem();
-        item3.name = "点点点";
-        item3.wallpaperId = UUID.randomUUID().toString();
-        item3.link = "http://www.kinglloy.com/";
-        item3.author = "Yalin";
-        item3.iconUrl = "http://172.22.158.86:6060/advance/icons/the_blue.png";
-        item3.downloadUrl = "http://172.22.158.86:6060/advance/components/point.component";
-        item3.checkSum = ChecksumUtil.getChecksum("./wallpapers/advance/components/point.component");
-        item3.providerName = "com.yalin.component.point.ProviderImpl";
+        AdvanceWallpaperItem pointItem = new AdvanceWallpaperItem();
+        pointItem.name = "点点点";
+        pointItem.wallpaperId = UUID.randomUUID().toString();
+        pointItem.link = "http://www.kinglloy.com/";
+        pointItem.author = "Yalin";
+        pointItem.iconUrl = RES_HOST + "/advance/icons/the_blue.png";
+        pointItem.downloadUrl = RES_HOST + "/advance/components/point.component";
+        pointItem.checkSum = ChecksumUtil.getChecksum("./wallpapers/advance/components/point.component");
+        pointItem.providerName = "com.yalin.component.point.ProviderImpl";
 
-        wallpapers.add(item);
-        wallpapers.add(item2);
-        wallpapers.add(item3);
+        AdvanceWallpaperItem rainbowItem = new AdvanceWallpaperItem();
+        rainbowItem.name = "Rainbow";
+        rainbowItem.wallpaperId = UUID.randomUUID().toString();
+        rainbowItem.link = "kinglloy.com";
+        rainbowItem.author = "Yalin";
+        rainbowItem.iconUrl = RES_HOST + "/advance/icons/rainbow.png";
+        rainbowItem.downloadUrl = RES_HOST + "/advance/components/rainbow.component";
+        rainbowItem.checkSum = ChecksumUtil.getChecksum("./wallpapers/advance/components/rainbow.component");
+        rainbowItem.providerName = "com.yalin.wallpaper.rainbow.ProviderImpl";
+
+        wallpapers.add(cubeItem);
+        wallpapers.add(blueItem);
+        wallpapers.add(pointItem);
+        wallpapers.add(rainbowItem);
 
         return wallpapers;
     }
